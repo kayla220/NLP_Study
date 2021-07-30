2be0066a6d2dc68aa243df49960f08236a4b103e
kayla220 <kayla5913@gmail.com>
2021-07-30 16:13:04 +0900

    ADD:download module

--- models/attention/attention_model.py
+++ models/attention/attention_model.py
@@ -6,10 +6,17 @@ import pandas as pd
 import tensorflow as tf
 import numpy as np
 import time
-import os
+import os, sys
 from sklearn import metrics
 # from visualize_attention import attentionDisplay
-# from process_figshare import download_figshare, process_figshare
+from process_figshare import download_figshare, process_figshare
+
+def main():
+    # Download the data from Figshare and cleansing and splitting it for use in training
+    # download_figshare()
+    # process_figshare()
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
