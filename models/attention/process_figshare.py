0daefea9ec0defcfc64994920620ba877f4aca7f
kayla220 <kayla5913@gmail.com>
2021-07-30 16:14:16 +0900

    EDIT: error handling

--- models/attention/process_figshare.py
+++ models/attention/process_figshare.py
@@ -25,7 +25,7 @@ from urllib.request import urlretrieve
 DEFAULT_DATA_DIR = 'data/'
 FIGSHARE_PATH = 'https://ndownloader.figshare.com/files/'
 FIGSHARE_URL_MAPPING = {
-    'toxicity_annotations.tsv': FIGSHARE_PATH + '7394539'
+    'toxicity_annotations.tsv': FIGSHARE_PATH + '7394539',
     'toxicity_annotated_comments.tsv': FIGSHARE_PATH + '7394542'
 }
 
@@ -56,54 +56,54 @@ def download_figshare(download_data_dir=DEFAULT_DATA_DIR):
       print('Figshare data alread exists.')
       return
 
-def process_figshare(input_data_dir=DEFAULT_DATA_DIR,
-                    output_data_dir=DEFAULT_DATA_DIR):
- """
-    Cleans and splits the toxicity data from Figshare.
-    Args:
-          * input_data_dir (string): if provided, the directory where the
-            Figshare tsvs are stored
-          * output_data_dir (string): if provided, the directory where the
-            output splits should be written
-  """
-  already_exist=True
-  for split in ['train', 'test', 'dev']:
+
+def process_figshare(input_data_dir=DEFAULT_DATA_DIR, output_data_dir=DEFAULT_DATA_DIR):
+    """
+       Cleans and splits the toxicity data from Figshare.
+       Args:
+             * input_data_dir (string): if provided, the directory where the
+               Figshare tsvs are stored
+             * output_data_dir (string): if provided, the directory where the
+               output splits should be written
+        """
+    already_exist = True
+    for split in ['train', 'test', 'dev']:
       if not os.path.join(os.path.join(output_data_dir, 'wiki_%s.csv' % split)):
           already_exist=False
-  if already_exist:
+    if already_exist:
       print('Processed files alread exist.')
       return
 
-  # process된 파일이 없는걸 확인 후 process 시작
-  print('Processing files...', end='')
-  toxicity_annotated_comments = pd.read_csv(
-    os.path.join(input_data_dir, 'toxicity_annotated_comments.tsv'),
-    sep='\t',
-    dtype={'rev_id': 'str'})
-  toxicity_annotations = pd.read_csv(
-    os.path.join(input_data_dir, 'toxicity_annotations.tsv'),
-    sep='\t',
-    dtype={'rev_id': 'str'})
+    # process된 파일이 없는걸 확인 후 process 시작
+    print('Processing files...', end='')
+    toxicity_annotated_comments = pd.read_csv(
+        os.path.join(input_data_dir, 'toxicity_annotated_comments.tsv'),
+        sep='\t',
+        dtype={'rev_id': 'str'})
+    toxicity_annotations = pd.read_csv(
+        os.path.join(input_data_dir, 'toxicity_annotations.tsv'),
+        sep='\t',
+        dtype={'rev_id': 'str'})
 
-  annotations_gped = toxicity_annotations.groupby(
+    annotations_gped = toxicity_annotations.groupby(
     'rev_id', as_index=False).agg({'toxicity': 'mean'})
-  all_data = pd.merge(
+    all_data = pd.merge(
     annotations_gped, toxicity_annotated_comments, on='rev_id')
 
-  all_data['comment'] = all_data['comment'].apply(lambda x: re.sub(
+    all_data['comment'] = all_data['comment'].apply(lambda x: re.sub(
     'NEWLINE_TOKEN|TAB_TOKEN', ' ', x))
 
-  all_data['is_toxic'] = all_data['toxicity'] > 0.5
+    all_data['is_toxic'] = all_data['toxicity'] > 0.5
 
-  # split into train, valid, test
-  wiki_splits = {}
-  for split in ['train', 'test', 'dev']:
+    # split into train, valid, test
+    wiki_splits = {}
+    for split in ['train', 'test', 'dev']:
       wiki_splits[split] = all_data.query('split == @split')
 
-  for split in wiki_splits:
+    for split in wiki_splits:
       wiki_splits[split].to_csv(
         os.path.join(output_data_dir, 'wiki_%s.csv' % split), index=False)
-  print('Done!')
+    print('Done!')
 
 
 # TODO(nthain): Add input and output dirs as flags.
