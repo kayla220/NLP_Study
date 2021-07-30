f42a119c7584a943387eea7340ea3f53a59abb0e
kayla220 <kayla5913@gmail.com>
2021-07-30 15:52:42 +0900

    ADD:download data from url

---
+++ models/attention/attention_model.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Embed-Encode-Attend-Predict
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import time
+import os
+from sklearn import metrics
+# from visualize_attention import attentionDisplay
+# from process_figshare import download_figshare, process_figshare
+
+if __name__ == "__main__":
+    sys.exit(main())
